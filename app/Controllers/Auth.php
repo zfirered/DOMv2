72f6341778894d5ea57f72db619f233bf2de2a2c
Ahmad Nurjati <anurjati16@gmail.com>
2021-01-08 23:51:55 +0700

    masih belom bisa login

--- app/Controllers/Auth.php
+++ app/Controllers/Auth.php
@@ -3,12 +3,32 @@
 namespace App\Controllers;
 
 use CodeIgniter\Controller;
+use App\Models\AuthModel;
 
 class Auth extends Controller
 {
-    public function register(){
-        $data = array{
-            'nama'
+    public function login()
+    {
+        $model = new AuthModel;
+        $table = 'user';
+        $nip = $this->request->getPost('nip');
+        $password = $this->request->getPost('password');
+        $row = $model->get_data_login($nip, $table);
+        if ($row == NULL){
+            session()->setFlashData('pesan', 'Nip anda salah!');
+            return redirect()->to('/');
+        } 
+        if (password_verify($password, $row->password)){
+                $data = array(
+                    'log' => TRUE,
+                    'nip' => $row->nip,
+                    'role' => $row->role,
+                );
+                session()->set($data);
+                session()->setFlashData('pesan', 'Berhasil Login!');
+                return redirect()->to('home');
         }
+                session()->setFlashData('pesan', 'Password Salah!');
+                return redirect()->to('/');
     }
 }
--- app/Views/frontend/login.php
+++ app/Views/frontend/login.php
@@ -29,10 +29,10 @@
     <div class="card">
       <div class="card-body login-card-body">
         <p class="login-box-msg">Sign in to start your session</p>
-
-        <form action="../../index3.html" method="post">
+        <?= session()->get('pesan')?>
+        <form action="/auth/login" method="post">
           <div class="input-group mb-3">
-            <input type="email" class="form-control" placeholder="Email">
+            <input type="text" class="form-control" name="nip" placeholder="nip">
             <div class="input-group-append">
               <div class="input-group-text">
                 <span class="fas fa-envelope"></span>
@@ -40,7 +40,7 @@
             </div>
           </div>
           <div class="input-group mb-3">
-            <input type="password" class="form-control" placeholder="Password">
+            <input type="password" class="form-control" name="password" placeholder="Password">
             <div class="input-group-append">
               <div class="input-group-text">
                 <span class="fas fa-lock"></span>
