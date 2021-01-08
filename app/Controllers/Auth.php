541c8c4669c595e948b83cb17b4434ff5516a42c
Ahmad Nurjati <anurjati16@gmail.com>
2021-01-08 14:09:49 +0700

    belom jadi

---
+++ app/Controllers/Auth.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers;
+
+use CodeIgniter\Controller;
+
+class Auth extends Controller
+{
+    public function register(){
+        $data = array{
+            'nama'
+        }
+    }
+}
--- app/Views/frontend/login.php
+++ app/Views/frontend/login.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>AdminLTE 3 | Log in</title>
+  <title>DOM | Log in</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -23,7 +23,7 @@
 <body class="hold-transition login-page">
   <div class="login-box">
     <div class="login-logo">
-      <a href="../../index2.html"><b>Admin</b>LTE</a>
+      <a href="../../index2.html"><b>DOM</b>LOGIN</a>
     </div>
     <!-- /.login-logo -->
     <div class="card">
@@ -64,22 +64,8 @@
           </div>
         </form>
 
-        <div class="social-auth-links text-center mb-3">
-          <p>- OR -</p>
-          <a href="#" class="btn btn-block btn-primary">
-            <i class="fab fa-facebook mr-2"></i> Sign in using Facebook
-          </a>
-          <a href="#" class="btn btn-block btn-danger">
-            <i class="fab fa-google-plus mr-2"></i> Sign in using Google+
-          </a>
-        </div>
-        <!-- /.social-auth-links -->
-
         <p class="mb-1">
           <a href="forgot-password.html">I forgot my password</a>
-        </p>
-        <p class="mb-0">
-          <a href="register.html" class="text-center">Register a new membership</a>
         </p>
       </div>
       <!-- /.login-card-body -->
